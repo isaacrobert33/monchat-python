d3b55bceb1b0f3d48c8169fbf1263f5129d480c2
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-03-24 22:27:55 +0100

    Refactor codebase

--- monchat_server_v2/utils.py
+++ monchat_server_v2/utils.py
@@ -259,6 +259,7 @@ def get_hexdigest(algorithm, salt, raw_password):
     using the given algorithm ('md5', 'sha1' or 'crypt').
     """
     raw_password, salt = smart_str(raw_password), smart_str(salt)
+
     if algorithm == "crypt":
         try:
             import crypt
@@ -269,7 +270,7 @@ def get_hexdigest(algorithm, salt, raw_password):
         return crypt.crypt(raw_password, salt)
 
     if algorithm == "md5":
-        return hashlib.md5(salt + raw_password).hexdigest()
+        return hashlib.md5(salt.encode() + raw_password.encode()).hexdigest()
     elif algorithm == "sha1":
         return hashlib.sha1(salt.encode() + raw_password.encode()).hexdigest()
     raise ValueError("Got unknown password algorithm type in password.")
@@ -348,13 +349,10 @@ def get_chat_socket_id(msg_sender, msg_recipient):
 def map_unread_count(data: list, user_id, group=False):
     mapped = []
 
-    if group:
-        for msg in data:
+    for msg in data:
+        if group:
             unread_count = MonchatMsg.objects.filter(read_by__user_id=user_id).count()
-            msg["unread_count"] = unread_count
-            mapped.append(msg)
-    else:
-        for msg in data:
+        else:
             recp = (
                 msg["msg_sender"]["user_id"]
                 if msg["direction"] == "inbound"
@@ -366,8 +364,8 @@ def map_unread_count(data: list, user_id, group=False):
                 msg_sender__user_id=recp,
                 msg_recipient__user_id=user_id,
             ).count()
-            msg["unread_count"] = unread_count
-            mapped.append(msg)
+        msg["unread_count"] = unread_count
+        mapped.append(msg)
 
     return mapped
 

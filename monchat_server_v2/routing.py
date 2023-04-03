acc0eaa2c35fc5eaba8abea38360548e5e8be66d
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-04-03 12:25:37 +0100

    General read receipt routing

--- monchat_server_v2/routing.py
+++ monchat_server_v2/routing.py
@@ -5,12 +5,14 @@ from .consumers import (
     ReadRecieptConsumer,
     GroupConsumer,
     TypingConsumer,
+    GeneralReadReceiptConsumer,
 )
 
 websocket_urlpatterns = [
     path("ws/chat/<slug:tunnel_id>/", ChatConsumer.as_asgi()),
     path("ws/online/", OnlineStatusConsumer.as_asgi()),
     path("ws/read_reciept/<slug:chat_id>/", ReadRecieptConsumer.as_asgi()),
+    path("ws/general_read_receipt/", GeneralReadReceiptConsumer.as_asgi()),
     path("ws/group/<slug:group_id>/", GroupConsumer.as_asgi()),
     path("ws/typing/", TypingConsumer.as_asgi()),
 ]

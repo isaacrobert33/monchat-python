abdd4810e7fd6a10833661ce50481823577f3026
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-04-03 12:18:03 +0100

    General read receipt consumer

--- monchat_server_v2/consumers.py
+++ monchat_server_v2/consumers.py
@@ -190,6 +190,28 @@ class OnlineStatusConsumer(AsyncWebsocketConsumer):
         print("Status updated")
 
 
+class GeneralReadReceiptConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "general_read_receipt"
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "gen_chat_status_change", **data}
+        )
+
+    async def gen_chat_status_change(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    async def disconnect(self, code):
+        self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+
 class ReadRecieptConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = self.scope["url_route"]["kwargs"]["chat_id"]

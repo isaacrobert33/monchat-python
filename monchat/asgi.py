239893e4c8cd3b39ca71b70af8bdedbdb768d639
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-05-11 11:05:46 +0100

    ...

--- monchat/asgi.py
+++ monchat/asgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monchat.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from monchat_server.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monchat.settings")
 
 django_asgi_app = get_asgi_application()
 
